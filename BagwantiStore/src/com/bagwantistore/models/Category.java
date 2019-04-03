bd549e6ab752c82771c98de50e7f146987829231
admin <admin@DEL-CP-FLM07.NIITEDUCATION.COM>
2019-04-03 12:33:56 +0530

    Category View and Add Done

---
+++ BagwantiStore/src/com/bagwantistore/models/Category.java
@@ -0,0 +1,29 @@
+package com.bagwantistore.models;
+
+public class Category {
+	private int categoryId;
+	private String categoryName;
+	private String categoryDesc;
+	public int getCategoryId() {
+		return categoryId;
+	}
+	public void setCategoryId(int categoryId) {
+		this.categoryId = categoryId;
+	}
+	public String getCategoryName() {
+		return categoryName;
+	}
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+	public String getCategoryDesc() {
+		return categoryDesc;
+	}
+	public void setCategoryDesc(String categoryDesc) {
+		this.categoryDesc = categoryDesc;
+	}
+	
+	
+	
+	
+}
